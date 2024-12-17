a997f0bd45e95f2b59ad9c16de9b4c40b665f893
vppclone <111286587+vppclone@users.noreply.github.com>
2024-12-17 12:02:33 +0700

    optimize by reduce lib size and more compression

--- static/vite.config.ts
+++ static/vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		minify: 'terser',
+		cssMinify: 'lightningcss',
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+				pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.trace']
+			}
+		}
+	}
 });
